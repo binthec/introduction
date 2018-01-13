c91dd9873a1697398e8dc15224f3fcf6fa39745b
binthec <binthec@gmail.com>
2018-01-14 00:56:56 +0900

    master
    ・devise を入れた。基本的な設定のみ。管理画面側のビューやルーティングはこれから。

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,15 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( front/animate.css )
+Rails.application.config.assets.precompile += %w( front/icomoon.css )
+Rails.application.config.assets.precompile += %w( front/bootstrap.css )
+Rails.application.config.assets.precompile += %w( front/style.css )
+
+# js
+Rails.application.config.assets.precompile += %w( front/modernizr-2.6.2.min.js )
+Rails.application.config.assets.precompile += %w( front/jquery.min.js )
+Rails.application.config.assets.precompile += %w( front/jquery.easing.1.3.js )
+Rails.application.config.assets.precompile += %w( front/bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( front/jquery.waypoints.min.js )
+Rails.application.config.assets.precompile += %w( front/main.js )
